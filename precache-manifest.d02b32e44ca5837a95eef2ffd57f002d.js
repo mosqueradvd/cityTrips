ba7ecd00f66fac688e2b6272cadef5a6d062f60f
David Mosquera <mosqueradavid@outlook.es>
2019-08-03 00:14:31 -0500

    Updates

---
+++ precache-manifest.d02b32e44ca5837a95eef2ffd57f002d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "36dd7dea4ee254f4fca779534a9ce31c",
+    "url": "/cityTrips/static/media/citynbuild.36dd7dea.png"
+  },
+  {
+    "revision": "232c0c7d53abd410a715",
+    "url": "/cityTrips/static/css/main.b1fe9b7b.chunk.css"
+  },
+  {
+    "revision": "da28a56c8c7c49a42020",
+    "url": "/cityTrips/static/js/runtime~main.da28a56c.js"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/cityTrips/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "a45f1bc4e750910c6899",
+    "url": "/cityTrips/static/js/2.a45f1bc4.chunk.js"
+  },
+  {
+    "revision": "232c0c7d53abd410a715",
+    "url": "/cityTrips/static/js/main.232c0c7d.chunk.js"
+  },
+  {
+    "revision": "70a331a1771f2d7a3d2bb32346f76074",
+    "url": "/cityTrips/static/media/buildings-logo.70a331a1.png"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/cityTrips/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "59ef6349f6b2007c9893f60e675315cf",
+    "url": "/cityTrips/static/media/buildingsh.59ef6349.png"
+  },
+  {
+    "revision": "a45f1bc4e750910c6899",
+    "url": "/cityTrips/static/css/2.6c92d650.chunk.css"
+  },
+  {
+    "revision": "b358782230a75108b9d83c619d1c1a9f",
+    "url": "/cityTrips/index.html"
+  }
+];
